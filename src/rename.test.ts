8660286c46503f1be43e35ca84a98b432a8c0ade
Mateja Petrovic <mateja176@gmail.com>
2019-04-09 21:54:26 +0200

    Moves scripts to src fodler

--- src/rename.test.ts
+++ src/rename.test.ts
@@ -1,6 +1,6 @@
 // fs throws when node types are unavailable in the current directory
-import * as fs from "fs-extra";
 import * as commander from "commander";
+import * as fs from "fs-extra";
 import transform from "./utils/transform";
 
 commander
@@ -10,7 +10,7 @@ commander
   .option(
     "-t, --transformations <array>",
     "Array of transformations to be applied in order",
-    "[]"
+    "[]",
   )
   .option("-R, --recursive", "Perform recursive rename")
   .parse(process.argv);
@@ -26,8 +26,8 @@ const rename = (currentDirectory: string) => (node: string) =>
     `${currentDirectory}/${node}`,
     `${currentDirectory}/${node.replace(
       new RegExp(match, flags),
-      transform(replace)(transformationsArray)
-    )}`
+      transform(replace)(transformationsArray),
+    )}`,
   );
 
 const renameRecursively = (currentDirectory: string) => (node: string) => {
@@ -41,5 +41,5 @@ const renameRecursively = (currentDirectory: string) => (node: string) => {
 };
 
 fs.readdirSync(currentDirectory).forEach(
-  recursive ? renameRecursively(currentDirectory) : rename(currentDirectory)
+  recursive ? renameRecursively(currentDirectory) : rename(currentDirectory),
 );
